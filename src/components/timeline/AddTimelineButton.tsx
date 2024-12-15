28f1f5f2645ac35a330856d5888924a54d24adde
ddangme <ssilver121@naver.com>
2024-12-15 20:27:53 +0900

    fix: add timeline button 위치 개선

--- src/components/timeline/AddTimelineButton.tsx
+++ src/components/timeline/AddTimelineButton.tsx
@@ -1,4 +1,5 @@
 import { Plus } from 'lucide-react';
+import {LAYOUT, PADDING} from "../../constants/layout";
 
 interface AddTimelineButtonProps {
     onClick: () => void;
@@ -8,7 +9,13 @@ export default function AddTimelineButton({ onClick }: AddTimelineButtonProps) {
     return (
         <button
             onClick={onClick}
-            className="fixed bottom-10 right-5 w-14 h-14 bg-[#735BF2] rounded-full flex items-center justify-center shadow-lg hover:bg-[#6344E3] transition-colors"
+            style={{
+                bottom: `calc(${LAYOUT.TAB_BAR_HEIGHT} + ${PADDING.MAIN.BOTTOM})`,
+                right: PADDING.MAIN.HORIZONTAL,
+                marginRight: `calc((100vw - min(100vw, ${LAYOUT.MAX_CONTENT_WIDTH})) / 2)`,
+                marginBottom: `calc((100vh - min(100vh, ${LAYOUT.MAX_CONTENT_HEIGHT})) / 2)`
+            }}
+            className="fixed w-14 h-14 bg-[#735BF2] rounded-full flex items-center justify-center shadow-lg hover:bg-[#6344E3] transition-colors"
         >
             <Plus className="w-6 h-6 text-white" />
         </button>
